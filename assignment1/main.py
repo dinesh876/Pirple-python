7b8076c8ab672044d4654881a003312e0a4443f1
dinesh876 <dineshadventure15@gmail.com>
2019-01-26 12:20:19 +0530

    Initial commit

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,12 +1,29 @@
-#song detail
+#PYTHON ASSIGNMENT1
+'''
+Author         : Dinesh
+published Date : 26-01-2019
+Description    : Information about songs
+'''
 Artist = 'One direction'
 Name ='little things'
-Genre  = 'Pop'
+Genre  = 'Pop-folk'
+Released='2-Dec-2012'
+Label  ='syco'
+Album  ="Take me home"
+songwriters="Ed Sheeran-Fiona Bevan"
+producer="Jake Gostling"
 DurationInSeconds = 240
 
-print(Artist)
-print(Name)
-print(Genre)
-print(DurationInSeconds)
+print('--------------------------------------')
+print("Artist\t\t\t",Artist)
+print('Name\t\t\t',Name)
+print('Genre\t\t\t',Genre)
+print('Released\t\t',Released)
+print('Label\t\t\t',Label)
+print('Album\t\t\t',Album)
+print('songwriters\t\t',songwriters)
+print('producer\t\t',producer)
+print('DurationInSeconds\t',DurationInSeconds)
+print('--------------------------------------')
 
 
