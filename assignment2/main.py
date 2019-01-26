c10d3dfec0ed673aaed9ca1b848fe0618f3fc329
dinesh876 <dineshadventure15@gmail.com>
2019-01-26 13:04:26 +0530

    Initial commit

---
+++ assignment2/main.py
@@ -0,0 +1,49 @@
+#PYTHON HOMEWORK ASSIGNMENT 2
+'''
+Author         : Dinesh
+published Date : 26-01-2019
+Description    : Information about songs
+'''
+# function definition
+def Artist():
+    return "One direction"
+
+def songname():
+    return "Little Thinngs"
+
+def Genre():
+    return "pop-folk"
+
+def Album():
+    return "Take me home"
+
+def DurationInSeconds():
+    return 420
+
+def Label():
+    return "Syco"
+
+def Songwriters():
+    return 'Ed Sheeran-Fiona Bevan'
+
+def Producer():
+    return 'Jake Gostling'
+
+def isReleased():
+    return True
+
+#function calls
+print('--------------------------------------')
+print("Artist\t\t\t",Artist())
+print('Name\t\t\t',songname())
+print('Genre\t\t\t',Genre())
+print('IsReleased\t\t',isReleased())
+print('Label\t\t\t',Label())
+print('Album\t\t\t',Album())
+print('songwriters\t\t',Songwriters())
+print('producer\t\t',Producer())
+print('DurationInSeconds\t',DurationInSeconds())
+print('--------------------------------------')
+
+
+
